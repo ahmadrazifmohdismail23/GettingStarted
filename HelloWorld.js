7ad25d4b6fcfac0f6c4585e5bccee644be67c18d
ahmadrazifmohdismail23 <ahmadrazifmohdismail23@gmail.com>
2022-10-07 16:06:54 +0800

    First Commit

---
+++ HelloWorld.js
@@ -0,0 +1,11 @@
+let greeting = "Hello World";
+const myname = "I am Razif";
+console.log(greeting);
+console.log(myname);
+console.log("This is my first code\n");
+
+console.log(greeting);
+console.log(myname);
+console.log("This is my first code");
+greeting = "Bye World";
+console.log(greeting);
